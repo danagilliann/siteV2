212beae1b0fc073ef59e221d8c7e64351b513950
danagilliann <danalee96@gmail.com>
2015-07-11 01:01:36 -0400

    first commit

---
+++ server/config/environment/development.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// Development specific configuration
+// ==================================
+module.exports = {
+  // MongoDB connection options
+  mongo: {
+    uri: 'mongodb://localhost/sitev2-dev'
+  },
+
+  seedDB: true
+};
